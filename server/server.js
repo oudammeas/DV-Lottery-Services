10b0783456026fe884c3edf88688675b1364ecd9
Oudam Meas <oudam.meas@gmail.com>
2020-07-15 00:47:15 -0400

    Build a basic Express server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+
+// Set port number for server to listen to
+const port = 3000
+
+// Tell server to listen to the specified port
+app.listen(port, (error) => {
+    if (error) {
+        console.log('Something went wrong', error)
+    } else {
+        console.log('Server is listening on port ' + port)
+    }
+})
+
+// Provide response to get request on '/'
+app.get('/', (request, response) => {
+    response.send("Hello World...........");
+})
