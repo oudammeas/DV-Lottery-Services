664c3d46068521f01a54161855afaf2c3ec30604
Oudam Meas <oudam.meas@gmail.com>
2020-07-18 16:40:51 -0400

    2

--- src/components/pages/WelcomePage/WelcomePage.jsx
+++ src/components/pages/WelcomePage/WelcomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Content } from "rsuite";
-import Header from "../../elements/navbar";
 import MainLayout from "../../layouts/MainLayout.js/MainLayout";
 
 const WelcomePage = () => {
