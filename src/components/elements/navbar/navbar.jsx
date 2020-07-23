dcf7658c9193f1fbcffdf110b2c3c1eec7c3cd00
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2020-07-22 20:31:35 -0400

    add Affix to navbar

--- src/components/elements/navbar/navbar.jsx
+++ src/components/elements/navbar/navbar.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 import { observer, inject } from "mobx-react";
-
+import { Affix } from "rsuite";
 const menu = [
   { title: "common.navigation.home", route: "/" },
   { title: "common.navigation.process", route: "/" },
@@ -19,14 +19,12 @@ const PageHeader = ({ commonStore }) => {
 
   const styles = {
     navbar: {
-      root: {},
       header: {
         padding: "1em",
         height: "5em",
         width: "100%",
         alignContent: "center",
         padding: "2em",
-        backgroundColor: "inherite",
         justifyContent: "left",
       },
       body: {
@@ -41,31 +39,33 @@ const PageHeader = ({ commonStore }) => {
 
   return (
     <Header>
-      <Navbar appearance="inverse" style={styles.navbar.root}>
-        <Navbar.Header style={styles.navbar.header}>
-          <Nav>
-            <a className="navbar-brand logo">
-              {t("common.navigation.brand-name")}
-            </a>
-          </Nav>
-        </Navbar.Header>
-        <Navbar.Body style={styles.navbar.body}>
-          <Nav activeKey={commonStore.activeNavMenu} onSelect={handleSelect}>
-            {menu.map((m) => (
-              <Nav.Item
-                eventKey={t(m.title)}
-                componentClass={Link}
-                to={t(m.route)}
-              >
-                {t(m.title)}
-              </Nav.Item>
-            ))}
-          </Nav>
-          <Nav pullRight>
-            <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
-          </Nav>
-        </Navbar.Body>
-      </Navbar>
+      <Affix>
+        <Navbar appearance="inverse">
+          <Navbar.Header style={styles.navbar.header}>
+            <Nav>
+              <a className="navbar-brand logo">
+                {t("common.navigation.brand-name")}
+              </a>
+            </Nav>
+          </Navbar.Header>
+          <Navbar.Body style={styles.navbar.body}>
+            <Nav activeKey={commonStore.activeNavMenu} onSelect={handleSelect}>
+              {menu.map((m) => (
+                <Nav.Item
+                  eventKey={t(m.title)}
+                  componentClass={Link}
+                  to={t(m.route)}
+                >
+                  {t(m.title)}
+                </Nav.Item>
+              ))}
+            </Nav>
+            <Nav pullRight>
+              <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+            </Nav>
+          </Navbar.Body>
+        </Navbar>
+      </Affix>
     </Header>
   );
 };
