14ac22934c3c95fd41e9e8887e4d13110814e29d
Oudam Meas <oudam.meas@gmail.com>
2021-02-01 01:12:26 -0500

    Authentication 06 Remove amplify auth

--- amplify/backend/function/dvlotteryservices64acc3ebPostConfirmation/src/add-to-group.js
+++ amplify/backend/function/dvlotteryservices64acc3ebPostConfirmation/src/add-to-group.js
@@ -1,28 +1,28 @@
-/* eslint-disable-line */ const aws = require('aws-sdk')
+/* eslint-disable-line */ const aws = require('aws-sdk');
 
 exports.handler = async (event, context, callback) => {
-  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' })
+  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
   const groupParams = {
     GroupName: process.env.GROUP,
     UserPoolId: event.userPoolId,
-  }
+  };
 
   const addUserParams = {
     GroupName: process.env.GROUP,
     UserPoolId: event.userPoolId,
     Username: event.userName,
+  };
+
+  try {
+    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
+  } catch (e) {
+    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
   }
 
   try {
-    await cognitoidentityserviceprovider.getGroup(groupParams).promise()
+    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
+    callback(null, event);
   } catch (e) {
-    await cognitoidentityserviceprovider.createGroup(groupParams).promise()
+    callback(e);
   }
-
-  try {
-    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise()
-    callback(null, event)
-  } catch (e) {
-    callback(e)
-  }
-}
+};
